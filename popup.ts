71f7840493873fa724417ba682500c8c34ae98ae
Ostrea <Ostrea@users.noreply.github.com>
2016-09-16 14:31:59 +0300

    Fix lint warnings and style issues.

--- popup.ts
+++ popup.ts
@@ -1,53 +1,58 @@
 /// <reference path="typings/main.d.ts" />
 "use strict";
 
-document.addEventListener('DOMContentLoaded',
-                          () => {
-                              var inputField = <HTMLInputElement> document.getElementById("word_to_look_up");
-                              chrome.runtime.onMessage.addListener(function(request, sender) {
-                                  if (request.action == "selectedText") {
-                                      inputField.value = request.source;
-                                      handleDefineButton();
-                                  }
-                              });
 
-                              chrome.tabs.executeScript(null, {
-                                  file: "content.js"
-                              }, function() {
-                                  // If you try and inject into an extensions page
-                                  // or the webstore/NTP you'll get an error
-                                  if (chrome.runtime.lastError) {
-                                      alert("There was an error injecting script: \n"
-                                          + chrome.runtime.lastError.message);
-                                  }
-                              });
+document.addEventListener("DOMContentLoaded",
+    () => {
+        const inputField = <HTMLInputElement>
+            document.getElementById("word_to_look_up");
+        chrome.runtime.onMessage.addListener((request, sender) => {
+            if (request.action === "selectedText") {
+                inputField.value = request.source;
+                lookUpButtonHandler();
+            }
+        });
 
-                              document.getElementById("define_button").addEventListener("click",
-                                  handleDefineButton, false);
+        chrome.tabs.executeScript(null, { file: "content.js" }, () => {
+            // If you try and inject into an extensions page
+            // or the webstore/NTP you'll get an error.
+            if (chrome.runtime.lastError) {
+                alert("There was an error injecting script: \n"
+                    + chrome.runtime.lastError.message);
+            }
+        });
 
-                              inputField.addEventListener("keyup",
-                                  (event: KeyboardEvent) => {
-                                      if (event.keyCode == 13) {
-                                          handleDefineButton();
-                                      }
-                                  }, false);
-                              inputField.focus();
-                              inputField.select();
-                          });
+        document.getElementById("define_button").addEventListener("click",
+            lookUpButtonHandler, false);
+
+        inputField.addEventListener("keyup", (event: KeyboardEvent) => {
+            const enterButtonCode = 13;
+            if (event.keyCode === enterButtonCode) {
+                lookUpButtonHandler();
+            }
+        }, false);
+
+        inputField.focus();
+        inputField.select();
+    });
+
 
 enum Language {
     British,
     American
 }
 
-function handleDefineButton(): void {
-    var word = (<HTMLInputElement> document.getElementById("word_to_look_up")).value;
 
-    var languageAsString = (<HTMLSelectElement> document.getElementById("language")).value;
-    var language: Language;
-    if (languageAsString == "american") {
+function lookUpButtonHandler(): void {
+    const word =
+        (<HTMLInputElement>document.getElementById("word_to_look_up")).value;
+
+    const languageAsString =
+        (<HTMLSelectElement>document.getElementById("language")).value;
+    let language: Language;
+    if (languageAsString === "american") {
         language = Language.American;
-    } else if (languageAsString == "british") {
+    } else if (languageAsString === "british") {
         language = Language.British;
     } else {
         alert("Wrong language!");
@@ -56,24 +61,30 @@ function handleDefineButton(): void {
     sendRequest(language, word);
 }
 
-function sendRequest(language: Language, word: string): void {
-    var languageStringValue = (language == Language.American) ? "american_english" : "english";
 
-    var request = new XMLHttpRequest();
+function sendRequest(language: Language, word: string): void {
+    const languageStringValue = (language === Language.American) ?
+        "american_english" : "english";
+
+    const request = new XMLHttpRequest();
     request.open("GET",
         "https://www.oxforddictionaries.com/" +
-        "search/?direct=1&multi=1&dictCode=" + languageStringValue + "&q=" + word);
+        "search/?direct=1&multi=1&dictCode=" + languageStringValue
+        + "&q=" + word);
     request.onload = () => {
-        if (request.status != 200) {
-            alert("Bad response from server." +
-                "Status: " + request.status);
+        if (request.status !== 200) {
+            alert("Bad response from server. Status: " + request.status);
             return;
         }
 
         renderStatus(parseHtml(request));
+
         addEventListenersForAudioElements();
-        document.getElementById("define_button").addEventListener("click", handleDefineButton, false);
-        (<HTMLInputElement> document.getElementById("word_to_look_up")).value = word;
+        document.getElementById("define_button").addEventListener("click",
+            lookUpButtonHandler, false);
+
+        (<HTMLInputElement>document.getElementById("word_to_look_up"))
+            .value = word;
     };
     request.onerror = () => {
         alert("Error when trying to send request!");
@@ -82,25 +93,29 @@ function sendRequest(language: Language, word: string): void {
     request.send();
 }
 
+
 function renderStatus(htmlFromDictionary: string): void {
-    var wordDefinitionDiv = document.getElementById("word_definition");
+    const wordDefinitionDiv = document.getElementById("word_definition");
     wordDefinitionDiv.innerHTML = htmlFromDictionary;
 }
 
+
 function parseHtml(request: XMLHttpRequest): string {
-    var body = document.createElement("body");
+    const body = document.createElement("body");
     body.innerHTML = request.responseText;
-    var wordDefinition = body.querySelectorAll("div.responsive_cell_center");
-    var css = <HTMLLinkElement> body.querySelector("link[type='text/css']");
-    return css.outerHTML + (<HTMLElement> wordDefinition.item(0)).innerHTML;
+    const wordDefinition = body.querySelectorAll("div.responsive_cell_center");
+    const css = <HTMLLinkElement>body.querySelector("link[type='text/css']");
+    return css.outerHTML + (<HTMLElement>wordDefinition.item(0)).innerHTML;
 }
 
+
 function addEventListenersForAudioElements(): void {
-    var elementsWithPlayClass = document.getElementsByClassName("audio_play_button");
-    for (var i = 0; i < elementsWithPlayClass.length; i++) {
-        elementsWithPlayClass.item(i).addEventListener("click", function() {
-            var srcMp3 = this.getAttribute("data-src-mp3");
-            var srcOgg = this.getAttribute("data-src-ogg");
+    const elementsWithPlayClass = document
+        .getElementsByClassName("audio_play_button");
+    for (let i = 0; i < elementsWithPlayClass.length; i++) {
+        elementsWithPlayClass.item(i).addEventListener("click", function () {
+            const srcMp3 = this.getAttribute("data-src-mp3");
+            const srcOgg = this.getAttribute("data-src-ogg");
 
             if (supportAudioHtml5()) {
                 playHtml5(srcMp3, srcOgg);
@@ -111,39 +126,48 @@ function addEventListenersForAudioElements(): void {
     }
 }
 
+
 function supportAudioHtml5() {
-    var audioTag  = document.createElement("audio");
+    const audioTag = document.createElement("audio");
     try {
-        return (!!(audioTag.canPlayType)
-        && ((audioTag.canPlayType("audio/mpeg") != "no" && audioTag.canPlayType("audio/mpeg") != "")
-        || (audioTag.canPlayType("audio/ogg") != "no" && audioTag.canPlayType("audio/ogg") != "" )));
-    } catch(e) {
+        if (!audioTag.canPlayType) {
+            return false;
+        }
+
+        const canPlayMpeg = audioTag.canPlayType("audio/mpeg") !== "no"
+            && audioTag.canPlayType("audio/mpeg") !== "";
+        const canPlayOgg = audioTag.canPlayType("audio/ogg") !== "no"
+            && audioTag.canPlayType("audio/ogg") !== "";
+        return canPlayMpeg || canPlayOgg;
+    } catch (e) {
         return false;
     }
 }
 
-var audio = null;
+
+let audio = null;
 function playHtml5(srcMp3, srcOgg) {
-    if (audio != null){
-        // PLODOMAINT-345: avoid overlapping
-        if (!audio.ended){
+    if (audio != null) {
+        if (!audio.ended) {
             audio.pause();
-            if(audio.currentTime > 0) {
-                audio.currentTime = 0
+            if (audio.currentTime > 0) {
+                audio.currentTime = 0;
             }
         }
     }
 
     // Use appropriate source.
     audio = new Audio("");
-    if (audio.canPlayType("audio/mpeg") != "no" && audio.canPlayType("audio/mpeg") != "") {
+    if (audio.canPlayType("audio/mpeg") !== "no"
+        && audio.canPlayType("audio/mpeg") !== "") {
         audio = new Audio(srcMp3);
-    } else if (audio.canPlayType("audio/ogg") != "no" && audio.canPlayType("audio/ogg") != "") {
+    } else if (audio.canPlayType("audio/ogg") !== "no"
+        && audio.canPlayType("audio/ogg") !== "") {
         audio = new Audio(srcOgg);
     }
 
     // Play
-    audio.addEventListener("error", function() {
+    audio.addEventListener("error", function () {
         alert("Apologies, the sound is not available.");
     });
     audio.play();
