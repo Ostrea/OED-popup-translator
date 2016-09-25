7efb2a08179801590ebf0fc4df0da96dce323589
Ostrea <Ostrea@users.noreply.github.com>
2016-09-25 11:49:26 +0300

    Add basic structure.

---
+++ src/oxford_dictionary_provider.ts
@@ -0,0 +1,10 @@
+"use strict";
+
+
+import { APP_ID, APP_KEY } from "./secrets";
+import { Entry } from "./entry_classes";
+
+
+export function lookUpWord(word: string): Entry[] {
+    return [];
+}
