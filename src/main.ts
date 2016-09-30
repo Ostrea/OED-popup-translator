8c597f0180b7fdf517838e7b347102a549881975
Ostrea <Ostrea@users.noreply.github.com>
2016-09-30 12:53:43 +0300

    Basic skeleton for async usage.

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,34 @@
 
 
 import { Entry, Sense } from "./entry_classes";
+import { lookUpWord } from "./oxford_dictionary_provider";
 
 
 declare function template(templateArgs: Object): string;
 
 
 export function main(): void {
-    const definitions = [
-        new Entry("Exclamation", [
-            new Sense(
-                "Used as a greeting or to begin a telephone conversation.",
-                [new Sense("Used to express surprise", undefined, "British")]
-            )
-        ]), new Entry("Verb", [
-            new Sense("Say or shout “hello”; greet someone.")
-        ], "[NO OBJECT]")
-    ];
+    lookUpWord("hello", "us", populateTemplate);
 
+    // const definitions = [
+    //     new Entry("Exclamation", [
+    //         new Sense(
+    //             "Used as a greeting or to begin a telephone conversation.",
+    //             [new Sense("Used to express surprise", undefined, "British")]
+    //         )
+    //     ]), new Entry("Verb", [
+    //         new Sense("Say or shout “hello”; greet someone.")
+    //     ], "[NO OBJECT]")
+    // ];
+}
+
+
+function populateTemplate(entries: Entry[]): void {
     const wordDefinitionDiv = document.getElementById("word-definition");
-    wordDefinitionDiv.innerHTML = template({
-        word: "hello",
-        region: "American", inWhichLanguageOtherUses: "Language or `also`",
-        otherSpellings: ["hallo", "hullo"], definitions
-    });
+    alert("Populate template!");
+    // wordDefinitionDiv.innerHTML = template({
+    //     word: "hello",
+    //     region: "American", inWhichLanguageOtherUses: "Language or `also`",
+    //     otherSpellings: ["hallo", "hullo"], definitions
+    // });
 }
--- src/oxford_dictionary_provider.ts
+++ src/oxford_dictionary_provider.ts
@@ -5,6 +5,36 @@ import { APP_ID, APP_KEY } from "./secrets";
 import { Entry } from "./entry_classes";
 
 
-export function lookUpWord(word: string): Entry[] {
+const BASE_URL = "https://od-api.oxforddictionaries.com:443/api/v1/";
+
+
+export function lookUpWord(word: string, region: string,
+    populateTemplate: (entries: Entry[]) => void) {
+    const lookUpUrl = BASE_URL + "entries/en/" + word.toLowerCase()
+        + "/regions=" + region;
+
+    const request = new XMLHttpRequest();
+    request.open("GET", lookUpUrl);
+    request.setRequestHeader("app_id", APP_ID);
+    request.setRequestHeader("app_key", APP_KEY);
+
+    request.onload = () => {
+        if (request.status !== 200) {
+            alert("Bad response from server. Status: " + request.status);
+            return;
+        }
+        const entries = processJson(request.responseText);
+        populateTemplate(entries);
+    };
+    request.onerror = () => {
+        alert("Error when trying to send request!");
+    };
+
+    request.send();
+}
+
+
+function processJson(json: string): Entry[] {
+    alert(json);
     return [];
 }
