239751570392f7fda5080ef4b63e4c0a2376a6cf
Ostrea <Ostrea@users.noreply.github.com>
2016-10-02 21:17:44 +0300

    Add logic to get word and search for it.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 "use strict";
 
 
-import { Entry, Sense } from "./entry_classes";
+import { Region, Entry } from "./entry_classes";
 import { lookUpWord } from "./oxford_dictionary_provider";
 
 
@@ -9,7 +9,37 @@ declare function template(templateArgs: Object): string;
 
 
 export function main(): void {
-    lookUpWord("hello", "us", populateTemplate);
+    const inputField = <HTMLInputElement>document
+        .getElementById("word-to-look-up");
+
+    chrome.runtime.onMessage.addListener((request, sender) => {
+        if (request.action === "selectedText") {
+            inputField.value = request.source;
+            lookUpButtonHandler();
+        }
+    });
+
+    chrome.tabs.executeScript(null, { file: "content_bundle.js" }, () => {
+        // If you try and inject into an extensions page
+        // or the webstore/NTP you'll get an error.
+        if (chrome.runtime.lastError) {
+            alert("There was an error injecting script: \n"
+                + chrome.runtime.lastError.message);
+        }
+    });
+
+    document.getElementById("define-button").addEventListener("click",
+        lookUpButtonHandler, false);
+
+    inputField.addEventListener("keyup", (event: KeyboardEvent) => {
+        const enterButtonCode = 13;
+        if (event.keyCode === enterButtonCode) {
+            lookUpButtonHandler();
+        }
+    }, false);
+
+    inputField.focus();
+    inputField.select();
 }
 
 
@@ -18,8 +48,6 @@ function populateTemplate(word: string, region: string,
     const wordDefinitionDiv = document.getElementById("word-definition");
     wordDefinitionDiv.innerHTML = template({
         word: word, region: region, entries: entries
-        // region: "American", inWhichLanguageOtherUses: "Language or `also`",
-        // otherSpellings: ["hallo", "hullo"], definitions
     });
 
     const audios = wordDefinitionDiv.getElementsByClassName("audio");
@@ -28,4 +56,13 @@ function populateTemplate(word: string, region: string,
             this.firstElementChild.play();
         };
     });
+}
+
+
+function lookUpButtonHandler(): void {
+    const word = (<HTMLInputElement>document
+        .getElementById("word-to-look-up")).value;
+    const region = <Region>(<HTMLSelectElement>document
+        .getElementById("language")).value;
+    lookUpWord(word, region, populateTemplate);
 }
